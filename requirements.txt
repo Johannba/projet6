bdb45ca074817020a85f21be9cfe535e4116d6fb
johann Bacha <johannbacha@gmail.com>
2022-06-07 08:32:57 +0200

    Dernier changements

---
+++ requirements.txt
@@ -0,0 +1,24 @@
+PROJET 6 : Développer une interface utilisateur pour une application web Python
+
+Création d'une interface web utilisant l'API "OCMovies-API" permettant d'afficher le meilleur film selon le score du site IMDB, Une catégorie des sept meilleurs films ainsi que trois autres catégories. Lors d'un clic sur un film, une fenêtre modale contenant l'image du film et ses informations doit s'afficher.
+
+
+
+Installation
+1 - L'installation et le fonctionnement de l'API "OCMovies-API" est disponible sur le lien github suivant :
+https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR
+
+2 - Copier dans un répertoire les éléments disponibles sur le lien github suivant :
+https://github.com/Johannba/projet6.git
+
+3 - Ouverture du site :
+index.html
+
+
+Auteurs :
+
+Johann Bacha
+
+Remerciements :
+
+Merci à Ranga Gonnage pour ses conseils, ça pédagogie et sa diplomatie.
--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const film_tete_affiche = async () => {
       await fetch_meilleurs();
       tete_affiche = document.querySelectorAll("#contenu-affiche img");
       tete_affiche[0].src = `${genres[0].image_url}`;
+      tete_affiche[0].id = `${genres[0].id}`;
       titre_affiche = document.querySelectorAll("h1 ");
       titre_affiche[0].innerHTML = `${genres[0].title}`;
 
@@ -21,17 +22,17 @@ const film_tete_affiche = async () => {
 film_tete_affiche();
 const film_mieux_noté = async () => {
       await fetch_meilleurs();
-      await modale_details_url();
-      // await modale_genre();
       section1 = document.querySelectorAll("#section1 img");
       for (var i = 0; i < section1.length; i++) {
             section1[i].src = `${genres[i].image_url}`;
+            section1[i].id = `${genres[i].id}`;
 
       }
 
       section2 = document.querySelectorAll("#section2 img");
       for (var i = 0; i < section2.length; i++) {
             section2[i].src = `${genres[i + 3].image_url}`;
+            section2[i].id = `${genres[i + 3].id}`;
       }
 
 };
@@ -51,10 +52,12 @@ const film_action = async () => {
       section3 = document.querySelectorAll("#section3 img");
       for (var i = 0; i < section3.length; i++) {
             section3[i].src = `${genres[i].image_url}`;
+            section3[i].id = `${genres[i].id}`;
       }
       section4 = document.querySelectorAll("#section4 img");
       for (var i = 0; i < section4.length; i++) {
             section4[i].src = `${genres[i + 3].image_url}`;
+            section4[i].id = `${genres[i + 3].id}`;
       }
 };
 film_action();
@@ -73,10 +76,12 @@ const film_Adventure = async () => {
       section5 = document.querySelectorAll("#section5 img");
       for (var i = 0; i < section5.length; i++) {
             section5[i].src = `${genres[i].image_url}`;
+            section5[i].id = `${genres[i].id}`;
       }
       section6 = document.querySelectorAll("#section6 img");
       for (var i = 0; i < section6.length; i++) {
             section6[i].src = `${genres[i + 3].image_url}`;
+            section6[i].id = `${genres[i + 3].id}`;
       }
 };
 film_Adventure();
@@ -95,54 +100,52 @@ const film_Drama = async () => {
       section7 = document.querySelectorAll("#section7 img");
       for (var i = 0; i < section7.length; i++) {
             section7[i].src = `${genres[i].image_url}`;
+            section7[i].id = `${genres[i].id}`;
       }
       section8 = document.querySelectorAll("#section8 img");
       for (var i = 0; i < section8.length; i++) {
             section8[i].src = `${genres[i + 3].image_url}`;
+            section8[i].id = `${genres[i + 3].id}`;
       }
 };
 film_Drama();
 
 
-const modale_details_url = async () => {
-      await fetch_meilleurs();
-      var details_url = `${genres[0].url}`;
-      fetch(details_url)
-            .then((resp) => resp.json())
-            .then((promise) => {
-                  details = promise;
-                  return details;
-            });
+const modale_details_url = async function (details_url) {
+      return fetch(details_url);
 };
-modale_details_url();
 
 const modale_tete_affiche = async () => {
-      await fetch_meilleurs();
-      await modale_details_url();
-      // Get modal element
-      var modal = document.getElementById('simpleModal');
-
       // Get open modal button
       var modalBtn = document.getElementById('modalBtn');
       // Get close button
       var closeBtn = document.getElementsByClassName('closeBtn')[0];
-      var image_affiche = document.querySelectorAll(".modal-body img");
-      image_affiche[0].src = `${genres[0].image_url}`;
-      var modale_details = document.querySelectorAll(".modal-body p");
-      modale_details[0].innerHTML = `<p><strong>TITRE :</strong> ${genres[0].title}<br></br><strong>GENRES : </strong> ${genres[0].genres}<br></br><strong> DATE DE SORTIE :</strong> ${details.date_published} <br></br><strong> SCORE :</strong> ${genres[0].imdb_score}<br></br><strong> REALISATEUR : </strong> ${genres[0].directors}<br></br><strong> ACTEUR :</strong> ${genres[0].actors}<br></br><strong> RATED : </strong> ${details.rated}<br></br><strong> DUREE :</strong> ${details.duration} min<br></br><strong> RESULTAT BOX OFFICE : </strong> ${details.worldwide_gross_income}<br></br><strong> PAYS ORIGINE :</strong> ${details.countries}<br></br><strong> RESUME :</strong> ${details.long_description}  </p></p>`;
-
+      const body = document.querySelector('body');
+      const modalPopup = document.querySelector('.image-modal-popup');
+      const modalElement = element =>
+            document.querySelector(`.image-modal-popup ${element}`);
 
       // Listen for open click
       modalBtn.addEventListener('click', openModal);
       // Listen for close click
       closeBtn.addEventListener('click', closeModal);
-      // Listen for outside click
-      window.addEventListener('click', outsideClick);
 
       // Open modal
       function openModal() {
-
-            modal.style.display = 'block';
+            var img = document.querySelectorAll(".tete-affiche img");
+            console.log(img[0].id);
+            var film_url = "http://127.0.0.1:8000/api/v1/titles/" + img[0].id;
+            var details_film = modale_details_url(film_url);
+            details_film
+                  .then((resp) => resp.json())
+                  .then((details_film) => {
+                        body.style.overflow = 'hidden';
+                        modalPopup.style.display = 'block';
+                        modalElement('h1').innerHTML = details_film.title;
+                        console.log(modalElement('h1'))
+                        modalElement('p').innerHTML = `<br><strong>GENRES : </strong></br>${details_film.genres}<br></br><strong> DATE DE SORTIE :</strong> ${details_film.date_published} <br></br><strong> SCORE :</strong> ${genres[0].imdb_score}<br></br><strong> REALISATEUR : </strong> ${genres[0].directors}<br></br><strong> ACTEUR :</strong> ${genres[0].actors}<br></br><strong> RATED : </strong> ${details_film.rated}<br></br><strong> DUREE :</strong> ${details_film.duration} min<br></br><strong> RESULTAT BOX OFFICE : </strong> ${details_film.worldwide_gross_income}<br></br><strong> PAYS ORIGINE :</strong> ${details_film.countries}<br></br><strong> RESUME :</strong> ${details_film.long_description}  </p></p>`;
+                        modalElement('img').src = img[0].src;
+                  });
       }
 
       // Close modal
@@ -168,7 +171,6 @@ const modale_genre = async () => {
       //dynamically selects all elements inside modal popup
       const modalElement = element =>
             document.querySelector(`.image-modal-popup ${element}`);
-      // section1 = document.querySelectorAll("#section1 img");
 
       const body = document.querySelector('body');
 
@@ -180,20 +182,24 @@ const modale_genre = async () => {
       const modalPopup = document.querySelector('.image-modal-popup');
       //loops over each modal content img and adds click event functionality
       lightboxImages.forEach(img => {
-            console.log(img);
             const data = img.dataset;
             img.addEventListener('click', e => {
-                  body.style.overflow = 'hidden';
-                  e.stopPropagation();
-                  modalPopup.style.display = 'block';
-                  modalElement('h1').innerHTML = details.title;
-                  console.log(modalElement('h1'))
-                  modalElement('p').innerHTML = `<br><strong>GENRES : </strong></br>${details.genres}<br></br><strong> DATE DE SORTIE :</strong> ${details.date_published} <br></br><strong> SCORE :</strong> ${genres[0].imdb_score}<br></br><strong> REALISATEUR : </strong> ${genres[0].directors}<br></br><strong> ACTEUR :</strong> ${genres[0].actors}<br></br><strong> RATED : </strong> ${details.rated}<br></br><strong> DUREE :</strong> ${details.duration} min<br></br><strong> RESULTAT BOX OFFICE : </strong> ${details.worldwide_gross_income}<br></br><strong> PAYS ORIGINE :</strong> ${details.countries}<br></br><strong> RESUME :</strong> ${details.long_description}  </p></p>`;
-                  // modalElement('a').href = data.url;
-                  // modalElement('.secondary-link').href = data.repo;
-                  modalElement('img').src = img.src;
+                  var film_url = "http://127.0.0.1:8000/api/v1/titles/" + img.id;
+                  var details_film = modale_details_url(film_url);
+                  details_film
+                        .then((resp) => resp.json())
+                        .then((details_film) => {
+                              body.style.overflow = 'hidden';
+                              e.stopPropagation();
+                              modalPopup.style.display = 'block';
+                              modalElement('h1').innerHTML = details_film.title;
+                              console.log(modalElement('h1'))
+                              modalElement('p').innerHTML = `<br><strong>GENRES : </strong></br>${details_film.genres}<br></br><strong> DATE DE SORTIE :</strong> ${details_film.date_published} <br></br><strong> SCORE :</strong> ${genres[0].imdb_score}<br></br><strong> REALISATEUR : </strong> ${genres[0].directors}<br></br><strong> ACTEUR :</strong> ${genres[0].actors}<br></br><strong> RATED : </strong> ${details_film.rated}<br></br><strong> DUREE :</strong> ${details_film.duration} min<br></br><strong> RESULTAT BOX OFFICE : </strong> ${details_film.worldwide_gross_income}<br></br><strong> PAYS ORIGINE :</strong> ${details_film.countries}<br></br><strong> RESUME :</strong> ${details_film.long_description}  </p></p>`;
+                              modalElement('img').src = img.src;
+                        });
             });
       });
 }
++
 
-modale_genre();
+      modale_genre();
